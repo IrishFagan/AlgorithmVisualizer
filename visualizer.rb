2d5924d15ac5a8e34084dfbe92b6eed963c53a2a
Irish Fagan <IrishFagan@live.com>
2019-12-25 15:32:46 -0800

    Implement checkGoal
    checkGoal checks the current x and y of drawSquares to see if that's the goal block
    If the goal is the current x and y then checkGoal returns true
    If not returns false

--- visualizer.rb
+++ visualizer.rb
@@ -2,21 +2,35 @@ require 'ruby2d'
 
 set width: 500, height: 500
 
-def newSquare(x,y,c)
-	s = Square.new
-	s.x = x*20
-	s.y = y*20
-	s.size = 20
-	s.color = [c,c,c,1]
+def newSquare(x,y,clr)
+	sqr = Square.new
+	sqr.x = x*20
+	sqr.y = y*20
+	sqr.size = 20
+	sqr.color = clr
+end
+
+def goalCheck(x,y)
+	goal = [22,3]
+	if goal == [x,y]
+		return true
+	end
+	return false
 end
 
 def drawSquares()
 	i = 0
 	j = 0
+	start = [0,0]
 	while i <= 25
 		while j <= 25
 			c = j*0.04
-			newSquare(i,j,c)
+			clr = [c,c,c,1]
+			newSquare(i,j,clr)
+			if goalCheck(i,j)
+				clr = [0,0.8,0.2,1]
+				newSquare(i,j,clr)
+			end
 			j += 1
 		end
 		j = 0
